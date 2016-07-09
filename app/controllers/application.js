965f9624fc21405dac0db1b5887647271a361793
Zahra Jabini <zahra.arch@gmail.com>
2016-07-09 15:47:55 -0400

    display past prompts in about section

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -1,6 +1,14 @@
 import Controller from 'ember-controller';
+import injectService from 'ember-service/inject';
 
 export default Controller.extend({
+  prompt: injectService(),
+
+  init() {
+    this._super();
+    this.set('pastPrompts', this.get('prompt').getPastPrompts());
+  },
+
   actions: {
     toggleAbout() {
       this.toggleProperty('isShowingAbout');
