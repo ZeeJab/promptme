49bc560bb35fdd24fb082cf08d14aee43e6c37f2
Tom Dale <tom@tomdale.net>
2016-07-11 17:38:33 +0100

    Rename index.html to 200.html

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -4,6 +4,11 @@ var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function(defaults) {
   var app = new EmberApp(defaults, {
+    outputPaths: {
+      app: {
+        html: '200.html'
+      }
+    }
     // Add options here
   });
 
